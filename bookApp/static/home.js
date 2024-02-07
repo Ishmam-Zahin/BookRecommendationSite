a810069cc8afc6679a6b450b59de08217d691653
zahin <mdishmamzahin@gmail.com>
2024-02-07 21:11:30 +0600

    fixed endpoint in js

--- bookApp/static/home.js
+++ bookApp/static/home.js
@@ -1,108 +1,110 @@
 const myApiKey = "AIzaSyDvwGOI6Fi7m7N3x_VeDaQt60C8iPZeruc"
 let endpoint
 let data
-function validateData(){
+function validateData() {
     endpoint = `https://www.googleapis.com/books/v1/volumes?q=`
     let isValid = false
 
     let title = document.getElementById("searchTitle").value.trim()
-    if(title !== ""){
+    if (title !== "") {
         isValid = true
         let tmpTitle = title.split(" ")
         title = ""
-        for(let i=0; i<tmpTitle.length; i++){
-            if(i !== (tmpTitle.length - 1)) title += "intitle:"+tmpTitle[i]+"+"
-            else title += "intitle:"+tmpTitle[i]
+        for (let i = 0; i < tmpTitle.length; i++) {
+            if (i !== (tmpTitle.length - 1)) title += "intitle:" + tmpTitle[i] + "+"
+            else title += "intitle:" + tmpTitle[i]
         }
-        endpoint += title
+        endpoint += `${title}+`
     }
     let author = document.getElementById("searchAuthor").value.trim()
-    if(author !==""){
+    if (author !== "") {
         isValid = true
         let tmpAuther = author.split(" ")
         author = ""
-        for(let i=0; i<tmpAuther.length; i++){
-            if(i !== (tmpAuther.length - 1)) author += "inauthor:"+tmpAuther[i]+"+"
-            else author += "inauthor:"+tmpAuther[i]
+        for (let i = 0; i < tmpAuther.length; i++) {
+            if (i !== (tmpAuther.length - 1)) author += "inauthor:" + tmpAuther[i] + "+"
+            else author += "inauthor:" + tmpAuther[i]
         }
-        endpoint += `+${author}`
+        endpoint += `${author}+`
     }
     let publisher = document.getElementById("searchPublisher").value.trim()
-    if(publisher !== ""){
+    if (publisher !== "") {
         isValid = true
         let tmpPublisher = publisher.split(" ")
         publisher = ""
-        for(let i=0; i<tmpPublisher.length; i++){
-            if(i !== (tmpPublisher.length - 1)) publisher += "inpublisher:"+tmpPublisher[i]+"+"
-            else publisher += "inpublisher:"+tmpPublisher[i]
+        for (let i = 0; i < tmpPublisher.length; i++) {
+            if (i !== (tmpPublisher.length - 1)) publisher += "inpublisher:" + tmpPublisher[i] + "+"
+            else publisher += "inpublisher:" + tmpPublisher[i]
         }
-        endpoint += `+${publisher}`
+        endpoint += `${publisher}+`
     }
     let subject = document.getElementById("searchSubject").value.trim()
-    if(subject !== ""){
+    if (subject !== "") {
         isValid = true
         let tmpSubject = subject.split(" ")
         subject = ""
-        for(let i=0; i<tmpSubject.length; i++){
-            if(i !== (tmpSubject.length - 1)) subject += "subject:"+tmpSubject[i]+"+"
-            else subject += "subject:"+tmpSubject[i]
+        for (let i = 0; i < tmpSubject.length; i++) {
+            if (i !== (tmpSubject.length - 1)) subject += "subject:" + tmpSubject[i] + "+"
+            else subject += "subject:" + tmpSubject[i]
         }
-        endpoint += `+${subject}`
+        endpoint += `${subject}+`
     }
+
+    if (endpoint.at(endpoint.length - 1) === "+") endpoint = endpoint.slice(0, endpoint.length - 1)
+    // endpoint += "&"
+
     let order = document.getElementById("order").value
     endpoint += `&printType=books&maxResults=30&orderBy=${order}&key=${myApiKey}`
 
-    if(isValid === false) document.getElementById("resultContainer").innerHTML = `<p class="intro">ENTER AT LEAST ONE FIELD</p>`
-    else{
+    if (isValid === false) document.getElementById("resultContainer").innerHTML = `<p class="intro">ENTER AT LEAST ONE FIELD</p>`
+    else {
         getData()
     }
 }
 
-async function getData(){
-    try{
+async function getData() {
+    try {
         let p = await fetch(endpoint)
         data = await p.json()
-        console.log(data)
+        console.log(endpoint)
         showResult()
     }
-    catch (err){
+    catch (err) {
         document.getElementById("resultContainer").innerHTML = err.message
     }
 }
 
-function showResult(){
+function showResult() {
     let element = document.getElementById("resultContainer")
-    if(data.items === undefined){
+    if (data.items === undefined) {
         element.innerHTML = `<p class="intro">INVALID SEARCH</p>`
     }
-    else{
+    else {
         element.innerHTML = `
-        ${
-            function start(){
+        ${function start() {
                 let str = ""
-                for(let i=0; i<data.items.length; i++){
+                for (let i = 0; i < data.items.length; i++) {
                     str += `
                     <div class="books">
                     <p><span onclick="showExtraInfo(${i})">${data.items[i].volumeInfo.title}</span></p>
-                    <section>${
-                        function img(){
+                    <section>${function img() {
                             let sImg = ""
-                            if(data.items[i].volumeInfo.imageLinks !== undefined) sImg = `<img src="${data.items[i].volumeInfo.imageLinks.thumbnail}" alt="no image">`
+                            if (data.items[i].volumeInfo.imageLinks !== undefined) sImg = `<img src="${data.items[i].volumeInfo.imageLinks.thumbnail}" alt="no image">`
                             else sImg = `Thumbnail not available`
                             return sImg
                         }()
-                    }</section>
+                        }</section>
                     </div>
                     `
                 }
                 return str
             }()
-        }
+            }
         `
     }
 }
 
-function showExtraInfo(index){
+function showExtraInfo(index) {
     pElement = document.getElementById("extraInfo")
     pElement.style.display = `flex`
     let grid1 = document.getElementById("grid-1")
@@ -112,14 +114,13 @@ function showExtraInfo(index){
     document.getElementById("eTitle").innerHTML = `${data.items[index].volumeInfo.title}`
 
     grid1.innerHTML = `
-    ${
-        function img(){
-                            let sImg = ""
-                            if(data.items[index].volumeInfo.imageLinks !== undefined) sImg = `<img src="${data.items[index].volumeInfo.imageLinks.thumbnail}" alt="no image">`
-                            else sImg = `Thumbnail not available`
-                            return sImg
-                        }()
-    }
+    ${function img() {
+            let sImg = ""
+            if (data.items[index].volumeInfo.imageLinks !== undefined) sImg = `<img src="${data.items[index].volumeInfo.imageLinks.thumbnail}" alt="no image">`
+            else sImg = `Thumbnail not available`
+            return sImg
+        }()
+        }
     `
 
     grid2.innerHTML = `
@@ -136,7 +137,7 @@ function showExtraInfo(index){
     `
 }
 
-function hideExtraInfo(){
+function hideExtraInfo() {
     pElement = document.getElementById("extraInfo")
     pElement.style.display = `none`
 }
